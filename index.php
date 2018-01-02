1d0187a3bdb9a974605d26a349617a461d67cb8f
polo <polodev10@gmail.com>
2018-01-02 17:03:57 +0600

    updated index file

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <?php 
-
 class Person {
-  const CITY = 'Feni';
-  public static $division = 'Chittagong';
   public $first_name;
   public $last_name;
-  public $email;
   public function fullName () {
-    return "{$this->first_name} {$this->last_name}'s mobile phone";
+    return $this->first_name . ' ' . $this->last_name;
   }
+  public static $city = 'feni';
+  const CITY = 'feni2';
 }
-echo Person::$division;
-
-// $arafat->first_name = 'Arafat';
-// $arafat->last_name = 'Ahmed';
-// $arafat->college = 'fgc';
-// $arafat->email = 'arafat@gmail.com';
-
-// print_r($arafat);
-// echo $arafat->fullName();
+$arafat = new Person;
+$arafat->first_name = 'arafat';
+$arafat->last_name = 'rahaman';
+echo Person::CITY;
+echo Person::$city;
 
 
