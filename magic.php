e9c3784ea2afd1c7b6c50c9250b4b31c45275dee
polo <polodev10@gmail.com>
2018-01-02 17:11:18 +0600

    update readme.md

---
+++ magic.php
@@ -0,0 +1,16 @@
+<?php
+class Person {
+  public function __construct ($first_name, $last_name) {
+    $this->first_name = $first_name;
+    $this->last_name = $last_name;
+  }
+  public $first_name;
+  public $last_name;
+  public function fullName () {
+    return $this->first_name . ' ' . $this->last_name;
+  }
+  public static $city = 'feni';
+  const CITY = 'feni2';
+}
+$arafat = new Person('arafat', 'rahman');
+echo $arafat->fullName();
